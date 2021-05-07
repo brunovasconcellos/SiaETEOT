6004215061c5452ee05d5fd42ce9905635c03b5c
Bruno Vasconcellos <brunovasconcellos133@gmail.com>
2021-05-06 21:10:59 -0300

    adiciona hierarquia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,130 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
+        
+        Gate::define("admin", function ($user) {
+
+            if ($user->level == 11) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
         Gate::define("diretoria", function ($user) {
 
-            if ($user->level >= 9) {
+            if ($user->level == 10) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("secretaria", function ($user) {
+
+            if ($user->level == 9) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+        
+        Gate::define("corpo_docente", function ($user) {
+
+            if ($user->level == 8) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("coordenacao", function ($user) {
+
+            if ($user->level == 7) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("supervisao", function ($user) {
+
+            if ($user->level == 6) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("inspetoria", function ($user) {
+
+            if ($user->level == 5) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("setor_pessoal", function ($user) {
+
+            if ($user->level == 4) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("orientacao_educacional", function ($user) {
+
+            if ($user->level == 3) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("responsavel", function ($user) {
+
+            if ($user->level == 2) {
+
+                return true;
+
+            }
+
+            return false;
+
+        });
+
+        Gate::define("aluno", function ($user) {
+
+            if ($user->level == 1) {
 
                 return true;
 
