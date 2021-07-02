7f5ed5e501900e5647cf3facb62df168e76aa4c1
RuanSessa <56142386+RuanSessa@users.noreply.github.com>
2021-07-01 23:44:35 -0300

    seed discipline

--- database/seeders/DisciplineSeeder.php
+++ database/seeders/DisciplineSeeder.php
@@ -17,20 +17,64 @@ class DisciplineSeeder extends Seeder
     {
         Discipline::insert([
             [
-                "discipline_name" => "Matematica",
-                "discipline_abbreviation" => "mat"
+                "discipline_name" => "Matemática",
+                "discipline_abbreviation" => "Mat"
             ],
             [
-                "discipline_name" => "Historia",
-                "discipline_abbreviation" => "hist"
+                "discipline_name" => "História",
+                "discipline_abbreviation" => "Hist"
             ],
             [
-                "discipline_name" => "Portugues",
-                "discipline_abbreviation" => "port"
+                "discipline_name" => "Língua Portuguesa",
+                "discipline_abbreviation" => "Port"
             ],
             [
                 "discipline_name" => "Filosofia",
-                "discipline_abbreviation" => "filo"
+                "discipline_abbreviation" => "Filo"
+            ],
+            [
+                "discipline_name" => "Sociologia",
+                "discipline_abbreviation" => "Socio"
+            ],
+            [
+                "discipline_name" => "Modelagem de dados 1",
+                "discipline_abbreviation" => "MD I"
+            ],
+            [
+                "discipline_name" => "Modelagem de dados 2",
+                "discipline_abbreviation" => "MD II"
+            ],
+            [
+                "discipline_name" => "Geografia",
+                "discipline_abbreviation" => "Geo"
+            ],
+            [
+                "discipline_name" => "Química",
+                "discipline_abbreviation" => "Quim"
+            ],
+            [
+                "discipline_name" => "Física",
+                "discipline_abbreviation" => "Fis"
+            ],
+            [
+                "discipline_name" => "Modelagem de dados 2",
+                "discipline_abbreviation" => "MD II"
+            ],
+            [
+                "discipline_name" => "Inglês",
+                "discipline_abbreviation" => "Ing"
+            ],
+            [
+                "discipline_name" => "Empreendedorismo",
+                "discipline_abbreviation" => "Emp"
+            ],
+            [
+                "discipline_name" => "Educação Física",
+                "discipline_abbreviation" => "ED. Física"
+            ],
+            [
+                "discipline_name" => "Biologia",
+                "discipline_abbreviation" => "Bio"
             ]
         ]);
     }
