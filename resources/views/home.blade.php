3eea9ef65f6da8610a638e8f6aafde3e142246ed
RuanSessa <56142386+RuanSessa@users.noreply.github.com>
2021-07-01 21:20:40 -0300

    home v1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,4 +16,39 @@
 
 @section('content')
 <span class='display-4'>Olá, Seja Bem Vindo {{Auth::user()->name}}</span>
+
+<br>
+<br>
+
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-header">Adicionar Funcionário</h5>
+          <p class="card-text">Ingresse um novo funcionário como uma nova função.</p>
+          <a href="{{url('dashboard/employee')}}" class="btn btn-primary">Adicionar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-header">Adicionar Aluno</h5>
+          <p class="card-text">ingresse um novo aluno na grade escolar.</p>
+          <a href="{{url('dashboard/student')}}" class="btn btn-primary">Adicionar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-header">Adicionar Responsável</h5>
+            <p class="card-text">Adicione um novo responsável legítimo de um aluno ao sistema.</p>
+            <a href="{{url('dashboard/responsible')}}" class="btn btn-primary">dicionar</a>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
 @stop
